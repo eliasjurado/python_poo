5c5d014bac5435921cf5f9d8f6ce6d8f399a0b93
Elias Jurado <i201611913@cibertec.edu.pe>
2022-05-01 06:53:30 -0500

    add depositar

--- sesion1/ejercicio1.py
+++ sesion1/ejercicio1.py
@@ -21,16 +21,23 @@ class User:
         self.dinero-=monto
         print(self.dinero)
 
+    def transferir( self,destino,monto):
+        self.retirar(monto)
+        destino.depositar(monto)
+
     def mostrarBalance(self):
         return f'Usuario: {self.nombre} - Saldo: {self.dinero}'
 
 objElias = User("Elias", 100)
+objJhomar = User("Jhomar", 100)
 # Depositamos 50 soles en la cuenta de mi usuario
-objElias.depositar(50)
+# objElias.depositar(50)
 # objElias.depositar(5)
 # Retiramos 4 soles para un helado
-objElias.retirar(4)
+# objElias.retirar(4)
+objElias.transferir(objJhomar,50)
 
 # Mostramos el balance
-respuestaBalance=objElias.mostrarBalance()
-print(respuestaBalance)
+respuestaBalanceElias=objElias.mostrarBalance()
+respuestaBalanceJhomar=objJhomar.mostrarBalance()
+print(respuestaBalanceElias+" "+respuestaBalanceJhomar)
